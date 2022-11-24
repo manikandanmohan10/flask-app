0786617a9e395f7e5b79454e5744b1f272153516
manikandanmohan10 <manikandan.mohan@softsuave.com>
2022-11-24 15:51:10 +0530

    procfile and slashes in endpoint

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT src:create_app
+web: gunicorn --bind 127.0.0.1:5000 src:create_app
--- src/blueprint/bp.py
+++ src/blueprint/bp.py
@@ -9,23 +9,23 @@ from src.api.wtf_form import WTFFormAPI
 
 class AuthBlueprint(Blueprint):
     def __init__(self):
-        super(AuthBlueprint, self).__init__("auth", __name__, url_prefix="/auth/v1/")
-        self.add_url_rule('register/', view_func=RegisterAPI.as_view("register"))
-        self.add_url_rule('login/', view_func=LoginAPI.as_view("login"))
-        self.add_url_rule('tokenCheck/', view_func=TokenCheckAPI.as_view("tokenCheck"))
-        self.add_url_rule('getAccessToken/', view_func=GetAccessTokenAPI.as_view("getAccessToken"))
+        super(AuthBlueprint, self).__init__("auth", __name__, url_prefix="/auth/v1")
+        self.add_url_rule('/register', view_func=RegisterAPI.as_view("register"))
+        self.add_url_rule('/login', view_func=LoginAPI.as_view("login"))
+        self.add_url_rule('/tokenCheck', view_func=TokenCheckAPI.as_view("tokenCheck"))
+        self.add_url_rule('/getAccessToken', view_func=GetAccessTokenAPI.as_view("getAccessToken"))
 
 
 class QuoteBlueprint(Blueprint):
     def __init__(self):
-        super(QuoteBlueprint, self).__init__("quote", __name__, url_prefix="/quote/")
-        self.add_url_rule('', view_func=AddQuoteAPI.as_view("quote"))
+        super(QuoteBlueprint, self).__init__("quote", __name__, url_prefix="/quote")
+        self.add_url_rule('/', view_func=AddQuoteAPI.as_view("quote"))
 
 
 class ToDoBluprint(Blueprint):
     def __init__(self):
-        super(ToDoBluprint, self).__init__("todo", __name__, url_prefix="/todo/")
-        self.add_url_rule('', view_func=ToDoAPI.as_view("todo"))
+        super(ToDoBluprint, self).__init__("todo", __name__, url_prefix="/todo")
+        self.add_url_rule('/', view_func=ToDoAPI.as_view("todo"))
         # self.add_url_rule('/<id>/', view_func=ToDoAPI.as_view("todo_delete"), methods=['PATCH', 'DELETE'])
         # self.add_url_rule('upd/<int:id>/', view_func=ToDoAPI.as_view("todo_update"))
         
